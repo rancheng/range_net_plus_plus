666cc0294e5a9f9f4bb43f6f5a78057eaca5b2c3
rancheng <752050943@qq.com>
2020-04-06 21:56:26 -0400

    fix bug when running under pytorch 1.3.x

--- tasks/semantic/modules/ioueval.py
+++ tasks/semantic/modules/ioueval.py
@@ -51,9 +51,13 @@ class iouEval:
             self.last_scan_size = idxs.shape[-1]
 
         # make confusion matrix (cols = gt, rows = pred)
-        self.conf_matrix = self.conf_matrix.index_put_(
-            tuple(idxs), self.ones, accumulate=True)
-
+        # https: // github.com / pytorch / pytorch / issues / 31672
+        # if you are using pytorch 1.3.1, please do not uncomment out this line due to bug at:+-
+        # self.conf_matrix = self.conf_matrix.index_put_(
+        #     tuple(idxs), self.ones, accumulate=True)
+        tmp_conf_matrix = np.zeros((self.n_classes, self.n_classes), dtype=np.int64)
+        np.add.at(tmp_conf_matrix, idxs.cpu().data.numpy(), 1)
+        self.conf_matrix = torch.from_numpy(tmp_conf_matrix)
         # print(self.tp.shape)
         # print(self.fp.shape)
         # print(self.fn.shape)
